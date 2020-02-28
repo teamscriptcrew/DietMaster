4a0723d850b51c4d61807e0f7309d62fefbfc282
jijinkh <jijinkharidas10@gmail.com>
2020-02-28 17:52:03 +0530

    initial setup

--- DietMaster-Project/dietmanager/forms.py
+++ DietMaster-Project/dietmanager/forms.py
@@ -7,15 +7,4 @@ class RegisterForm(forms.Form):
     password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    phone_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control'}), required=False)
-
-class HealthForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    age = forms.IntegerField()
-    isMale = forms.BooleanField()
-    isnotMale = forms.BooleanField()
-    weight = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control'}))
-    height = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control'}))
-    isActive = forms.BooleanField()
-    isnotActive = forms.BooleanField()
-    isModeratelyActive = forms.BooleanField()
+    phone_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control'}))
--- DietMaster-Project/dietmanager/models.py
+++ DietMaster-Project/dietmanager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class HealthModel(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    age = models.IntegerField()
+    isMale = models.BooleanField()
+    isFemale = models.BooleanField()
+    weight = models.CharField(max_length=30)
+    height = models.CharField(max_length=30)
+    isActive = models.BooleanField()
+    isnotActive = models.BooleanField()
+    isModeratelyActive = models.BooleanField()
